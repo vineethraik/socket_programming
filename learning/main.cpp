19a92efb4dbf621744f94531c83d383f15c43d73
vineethraik <vineethraik23082000@gmail.com>
2021-04-29 19:31:20 +0530

    updating

--- learning/main.cpp
+++ learning/main.cpp
@@ -5,7 +5,7 @@
 int count=0;
 struct sockaddr_in source,dest;
 
-
+data d;
  
 int i,j;
 SOCKET sock;
@@ -13,23 +13,26 @@ int main(){
     char hostname[100];
     struct hostent *local;
     struct in_addr addr;
+
     int in;
+
+    //wsa startup
     WSADATA wsaData;
     WORD dllversion=MAKEWORD(2,1);
     if(( i=WSAStartup(dllversion,&wsaData))!=0)printf("wsa startup error %d",i), ExitProcess(EXIT_FAILURE);
 
+    //opening a socket
     sock=socket(AF_INET,SOCK_RAW,IPPROTO_IP);
     if(sock<=0)printf("sock error %d",sock),ExitProcess(EXIT_FAILURE);
 
+    //reading hostname and gathering ip of interfaces
     if (gethostname(hostname, sizeof(hostname)) == SOCKET_ERROR)
 	{
 		printf("Error : %d",WSAGetLastError());
 		return 1;
 	}
 	printf("\nHost name : %s \n",hostname);
-
     local =gethostbyname(hostname);
-
    if (local == NULL)
 	{
 		printf("Error : %d.\n",WSAGetLastError());
@@ -45,6 +48,7 @@ int main(){
 	printf("Enter the interface number you would like to sniff : ");
 	scanf("%d",&in);
 
+    //binding to chosed interface
     memset(&dest, 0, sizeof(dest));
 	memcpy(&dest.sin_addr.s_addr,local->h_addr_list[in],sizeof(dest.sin_addr.s_addr));
 	dest.sin_family = AF_INET;
@@ -56,6 +60,7 @@ int main(){
 		return 1;
 	}
 
+    //setting the socket to sniffing mode(recive all trafic in the interface)
     j=1;
 	if (WSAIoctl(sock, SIO_RCVALL, &j, sizeof(j), 0, 0, (LPDWORD) &in , 0 , 0) == SOCKET_ERROR)
 	{
@@ -63,8 +68,38 @@ int main(){
 		return 1;
 	}
 
-    capture(sock);
+    //capturing and processing packets one by one
+    capture_data c_d;
+    capture_info c_info;
+    data d;
+    memcpy(&d.host.s_addr,local->h_addr_list[in],sizeof(d.host.s_addr));
+    while(1){
+        c_d=capture(sock);
 
+        int protocol=get_protocol(c_d);
+        c_info.total++;
+
+        switch(protocol){
+        case 1:
+        c_info.icmp++;
+        break;
+        case 2:
+        c_info.igmp++;
+        case 6:
+        c_info.tcp++;
+        gettcpdata(c_d,d);
+        break;
+        case 17:
+        c_info.udp++;
+        getudpdata(c_d,d);
+        break;
+        default:
+        c_info.other++;
+        break;
+        }
+        c_info.print();
+    }
+    
 
 
     closesocket(sock);
